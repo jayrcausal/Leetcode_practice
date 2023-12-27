53be7deec523de2e7cbb296ae2e5dd6f3fb714fd
JR <90957797+GostabMath@users.noreply.github.com>
2023-12-26 21:34:30 -0500

    Practice as of 12/26

--- Leetcode practice for DB.py
+++ Leetcode practice for DB.py
@@ -1,4 +1,144 @@
 #leetcode practice memo:
+#loop practice: 
+class Solution1:
+    def countPoints(self, points: list[List[int]], queries: list[List[int]]) -> list[int]:
+        answer = []
+        for q in queries:
+            i=0
+            for p in points:
+                if (p[0]-q[0])**2 + (p[1]-q[1])**2 <= q[2]**2:
+                    i+=1
+            answer.append(i)
+        
+        return answer
+    
+class EfficientSolution1:
+    #
+    def countPoints(self, points: list[list[int]], queries: list[list[int]]) -> list[int]:
+        res = []
+        for x,y,r in queries:
+            count = 0
+            for a,b in points:
+                if (x-a)*(x-a) + (y-b)*(y-b) <= r*r:
+                    count += 1
+            res.append(count)
+        return res
+    
+#In Code 1, you directly unpack the elements (x, y, r) from each query in the loop header, 
+#while in Code 2, you first iterate over the queries as q and then access its elements using x, y, r = q.
+    
+#loop for lattice points: variation
+class Solution2:
+    def countLatticePoints(self, circles: list[list[int]]) -> int:
+        answer = set()
+        for x,y,r in circles: #iterate each (x,y,r)
+            for i in range(x-r,x+r+1): # find all integer points for the point (x,y,r)
+                for j in range(y-r,y+r+1):
+                    if (x-i)**2 +(y-j)**2 <= r**2:
+                        answer.add((i,j))
+        return answer
+    
+Lattice = Solution2()
+print(Lattice.countLatticePoints([[2,2,2],[3,4,1]]))
+
+#add two sums
+
+
+l1 = [9,9,9,9,9,9,9,9]
+l2 = [9,9,9,9]
+n1=len(l1)
+n2 = len(l2)
+answer = 0
+
+for i in range(n1):
+    answer = answer + l1[i]*10**i+l2[i]*10**i
+
+print(answer)
+
+string = "abcdkel"
+print(string[5] == string[6])
+
+#longest substring without repeated characters.
+
+class Solution7:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        l, r = 0, 0
+        dic = {}
+        res = 0
+        for r in range(len(s)):
+            if s[r] in dic and dic[s[r]] >= l:
+                l = dic[s[r]] + 1
+            res = max(res, r - l + 1) # why do we need this?
+            dic[s[r]] = r
+        return res
+    
+
+#Review the ListNode: how they are structured and how they should be accessed. 
+# Merge SOrt:
+class SolutionMergeSort:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        a,b,write_index = m-1, n-1, m+n-1
+        while b>=0:
+            if a>=0 and nums1[a] > nums2[b]:
+                nums1[write_index] = nums1[a]
+                a-=1
+            else:
+                nums1[write_index] = nums2[b]
+                b-=1
+            write_index -=1
+
+# let break down the cases;
+#else part include all cases for a <0, all cases for a >=0 but nums1[a]<=nums2[b]
+#if a becomes less 0, which means all elements in nums1 (index <=m-1) have been sorted in the entire array.
+#just imagine : nums1 [3,5,9,0,0,0,0] nums2 [1,5,6,7] a = 3-1=2, b =4-1=3 , index = 3+4-1=6
+#iteration1 : [3,5,9,0,0,0,9], update a =1, b=3, index 5
+#interation2 : [3,5,9,0,0,7,9] update a=0, b=2 index =4
+#iteration 3 : [3,5,9,0,6,7,9] update a =0, b=1, index =3
+#iteration 4 : [3,5,9,5,6,7,9] update a =0 b=0, index =2
+#interation 5 [3,5,5,5,6,7,9] update a= 0 b=-1
+#interation 6 [3,3,5,5,6,7,9] a=-1,b =0
+# when a <0, else part executes and num1[0] was correctly replaced by num2[0]
+#results = [1,3,5,5,6,7,9]
+
+#Remove elements
+class SolutionRemove1:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0
+        l = len(nums)
+        i=0
+        while i <= l-1:
+            if nums[i] == val:
+                nums.remove(nums[i])
+                l-=1
+            else:
+                i+=1
+                k+=1
+        return k
+
+#break down cases:
+#nums =[3,2,2,3], val = 3,l=4, i =0,k=0
+#s1: 3==3. remove, nums = [2,2,3] i = 0 l=3
+#s2 : 2 !=3,not remove nums = [2,2,3], i=1 ,l=3 k=1
+#s3 : 2!= 3, not remove, nums= [2,2,3], i =2 l=3, k=2
+#s4: 3==3, remove, sums = [2,2], 
+class SolutionRemove2:
+    def removeElement(self, nums: list[int], val: int) -> int:
+        i = 0
+        n = len(nums)
+        
+        for j in range(n):
+            if nums[j] != val:
+                nums[i] = nums[j]
+                i += 1
+        
+        return i
+#this is more efficient
+
+
+
 #create a rep matrix using input index:
 matrix = [[6,3,5,9],
           [7,6,7,4],
@@ -148,3 +288,68 @@ class Solution7:
             res = max(res, r - l + 1) # why do we need this?
             dic[s[r]] = r
         return res
+    
+
+#Review the ListNode: how they are structured and how they should be accessed. 
+# Merge SOrt:
+class SolutionMergeSort:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        a,b,write_index = m-1, n-1, m+n-1
+        while b>=0:
+            if a>=0 and nums1[a] > nums2[b]:
+                nums1[write_index] = nums1[a]
+                a-=1
+            else:
+                nums1[write_index] = nums2[b]
+                b-=1
+            write_index -=1
+
+# let break down the cases;
+#else part include all cases for a <0, all cases for a >=0 but nums1[a]<=nums2[b]
+#if a becomes less 0, which means all elements in nums1 (index <=m-1) have been sorted in the entire array.
+#just imagine : nums1 [3,5,9,0,0,0,0] nums2 [1,5,6,7] a = 3-1=2, b =4-1=3 , index = 3+4-1=6
+#iteration1 : [3,5,9,0,0,0,9], update a =1, b=3, index 5
+#interation2 : [3,5,9,0,0,7,9] update a=0, b=2 index =4
+#iteration 3 : [3,5,9,0,6,7,9] update a =0, b=1, index =3
+#iteration 4 : [3,5,9,5,6,7,9] update a =0 b=0, index =2
+#interation 5 [3,5,5,5,6,7,9] update a= 0 b=-1
+#interation 6 [3,3,5,5,6,7,9] a=-1,b =0
+# when a <0, else part executes and num1[0] was correctly replaced by num2[0]
+#results = [1,3,5,5,6,7,9]
+
+#Remove elements
+class SolutionRemove1:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0
+        l = len(nums)
+        i=0
+        while i <= l-1:
+            if nums[i] == val:
+                nums.remove(nums[i])
+                l-=1
+            else:
+                i+=1
+                k+=1
+        return k
+
+#break down cases:
+#nums =[3,2,2,3], val = 3,l=4, i =0,k=0
+#s1: 3==3. remove, nums = [2,2,3] i = 0 l=3
+#s2 : 2 !=3,not remove nums = [2,2,3], i=1 ,l=3 k=1
+#s3 : 2!= 3, not remove, nums= [2,2,3], i =2 l=3, k=2
+#s4: 3==3, remove, sums = [2,2], 
+class SolutionRemove2:
+    def removeElement(self, nums: list[int], val: int) -> int:
+        i = 0
+        n = len(nums)
+        
+        for j in range(n):
+            if nums[j] != val:
+                nums[i] = nums[j]
+                i += 1
+        
+        return i
+#this is more efficient
